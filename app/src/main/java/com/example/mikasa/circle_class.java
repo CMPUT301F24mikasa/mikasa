f515fe665a340c5c896bae3bf42c1a3ab6bd778c
adnan-sami-raiyan <adnansamiraiyan2018@gmail.com>
2024-09-25 10:13:51 -0400

    added circle class

---
+++ app/src/main/java/com/example/mikasa/circle_class.java
@@ -0,0 +1,11 @@
+public class Circle extends Shape {
+		private string shape_name = "circle";
+
+		public string get_name(){
+			return shape_name;
+		}
+
+		public string set_name(String new_name){
+			this.name = new_name;
+		}
+}
